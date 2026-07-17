56a21194df20635a96916bf3b89d353f6247859b
Sam <samuelosita600@gmail.com>
2026-07-17 01:03:16 +0000

    Create Memory Service

---
+++ engines/memory/MemoryService.ts
@@ -0,0 +1,24 @@
+import { MemoryRecord } from "@/types";
+
+export class MemoryService {
+  public async save(memory: MemoryRecord): Promise<void> {
+      console.log("Saving memory:", memory);
+        }
+
+          public async search(query: string): Promise<MemoryRecord[]> {
+              console.log("Searching memories:", query);
+                  return [];
+                    }
+
+                      public async getAll(): Promise<MemoryRecord[]> {
+                          return [];
+                            }
+
+                              public async delete(id: string): Promise<void> {
+                                  console.log("Deleting memory:", id);
+                                    }
+                                    }
+
+                                    const memoryService = new MemoryService();
+
+                                    export default memoryService;
